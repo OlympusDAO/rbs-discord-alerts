73cb7c549d5fd60bd106f7c38c0fb2e13ef8aaff
Jem <0x0xjem@gmail.com>
2022-11-18 21:19:19 +0400

    Use production version of protocol-metrics subgraph

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,4 @@
 export const RBS_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/olympusdao/rbs";
 
-// TODO update protocol metrics
-// export const PROTOCOL_METRICS_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/olympusdao/olympus-protocol-metrics";
 export const PROTOCOL_METRICS_SUBGRAPH_URL =
-  "https://api.studio.thegraph.com/query/28103/olympus-protocol-metrics/3.0.35";
+  "https://api.thegraph.com/subgraphs/name/olympusdao/olympus-protocol-metrics";
