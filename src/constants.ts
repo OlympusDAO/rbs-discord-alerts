89cf3ca9ba7d31053bddc88b663cbfa0cd698175
Jem <0x0xjem@gmail.com>
2022-11-18 17:55:31 +0400

    Update subgraph URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // export const RBS_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/olympusdao/rbs";
-export const RBS_SUBGRAPH_URL = "https://api.studio.thegraph.com/query/28103/rbs/0.0.7";
+export const RBS_SUBGRAPH_URL = "https://api.studio.thegraph.com/query/28103/rbs/0.0.17";
 
 // export const PROTOCOL_METRICS_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/olympusdao/olympus-protocol-metrics";
 export const PROTOCOL_METRICS_SUBGRAPH_URL =
