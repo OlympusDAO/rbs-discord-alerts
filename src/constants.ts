04921a1e17cb1d6caa173327cbde5ccdff682ea9
Jem <0x0xjem@gmail.com>
2022-11-18 20:52:08 +0400

    Production version for RBS subgraph, mainnet contract address

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-// export const RBS_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/olympusdao/rbs";
-export const RBS_SUBGRAPH_URL = "https://api.studio.thegraph.com/query/28103/rbs/0.0.17";
+export const RBS_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/olympusdao/rbs";
 
+// TODO update protocol metrics
 // export const PROTOCOL_METRICS_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/olympusdao/olympus-protocol-metrics";
 export const PROTOCOL_METRICS_SUBGRAPH_URL =
   "https://api.studio.thegraph.com/query/28103/olympus-protocol-metrics/3.0.35";
