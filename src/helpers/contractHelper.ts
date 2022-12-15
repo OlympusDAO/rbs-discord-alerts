f97be0acf6ce52602779ea301a71c16149c83767
Jem <0x0xjem@gmail.com>
2022-12-15 14:42:57 +0400

    Send an alert when a heartbeat is late

--- src/helpers/contractHelper.ts
+++ src/helpers/contractHelper.ts
@@ -1,3 +1,7 @@
 export const getEtherscanTransactionUrl = (transaction: string, chain: string): string => {
   return `https://${chain === "Goerli" ? "goerli." : ""}etherscan.io/tx/${transaction}`;
 };
+
+export const getEtherscanAddressUrl = (address: string, chain: string): string => {
+  return `https://${chain === "Goerli" ? "goerli." : ""}etherscan.io/address/${address}`;
+};
